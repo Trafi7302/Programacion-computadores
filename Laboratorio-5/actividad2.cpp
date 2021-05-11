3b94590d0759f1af0b69c2a072f8516fd82489fa
Trafi7302 <74743115+Trafi7302@users.noreply.github.com>
2021-05-11 11:12:36 -0400

    Add files via upload

---
+++ Laboratorio-5/actividad2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char caracter = 0;
+
+int valor = 0;
+
+int main() {
+    printf("ingrese un carácter\n");
+    scanf("%c", & caracter);
+    valor = caracter;
+    printf("el carácter ingresado es representado por el valor de %i", valor);
+
+    return 0;
+}
